d3d7f1961d13ac287dc21cedc94342d4d2ebfbfc
Daniel Vikstrom <daniel.vikstrom@astek.net>
2025-04-28 15:13:47 +0200

    Rework step

--- components/lorawan/__init__.py
+++ components/lorawan/__init__.py
@@ -1,27 +1,13 @@
-import esphome.codegen as cg
-import esphome.config_validation as cv
 from esphome import automation
-from esphome.const import CONF_ID
+import esphome.codegen as cg
 
-# Definiera ditt C++ namespace och klass
 lorawan_ns = cg.esphome_ns.namespace('lorawan')
 LoRaWANComponent = lorawan_ns.class_('LoRaWANComponent', cg.Component)
 
-CONF_DEV_ADDR = "dev_addr"
-CONF_NWK_SKEY = "nwk_skey"
-CONF_APP_SKEY = "app_skey"
-
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(LoRaWANComponent),
-    cv.Required(CONF_DEV_ADDR): cv.uint32_t,
-    cv.Required(CONF_NWK_SKEY): cv.hex_bytes(16),
-    cv.Required(CONF_APP_SKEY): cv.hex_bytes(16),
-}).extend(cv.COMPONENT_SCHEMA)
+CONFIG_SCHEMA = cg.Schema({
+    cg.GenerateID(): cg.declare_id(LoRaWANComponent),
+})
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
+    var = cg.new_Pvariable(config[cg.CONF_ID])
     await cg.register_component(var, config)
-
-    cg.add(var.session_.dev_addr) = config[CONF_DEV_ADDR]
-    cg.add(cg.raw_expression(f'memcpy({var.get_object_name()}.session_.nwk_skey, {config[CONF_NWK_SKEY]}, 16);'))
-    cg.add(cg.raw_expression(f'memcpy({var.get_object_name()}.session_.app_skey, {config[CONF_APP_SKEY]}, 16);'))
--- components/lorawan/lorawan_component.h
+++ components/lorawan/lorawan_component.h
@@ -1,24 +1,40 @@
 #pragma once
 
 #include "esphome/core/component.h"
-#include "lorawan.h"
+#include "lorawan_packet_builder.h"
 
 namespace esphome {
 namespace lorawan {
 
 class LoRaWANComponent : public Component {
  public:
-  LoRaWANComponent() = default;
-
-  void setup() override;
+  void setup() override {}
   void loop() override {}
 
-  void send_unconfirmed_data(const uint8_t *payload, size_t length);
+  void set_session(const LoRaWANSessionKeys &session) {
+    this->session_ = session;
+  }
 
-  Session session_;
+  void publish_payload(const uint8_t *payload, size_t length) {
+    PacketBuilder builder(this->session_);
+    std::vector<uint8_t> packet;
+
+    if (builder.build_uplink(payload, length, packet)) {
+      send_packet(packet.data(), packet.size());
+      this->session_.frame_counter++;
+    } else {
+      ESP_LOGE(TAG, "Failed to build LoRaWAN packet");
+    }
+  }
 
  protected:
-  PacketBuilder *builder_;
+  LoRaWANSessionKeys session_;
+
+  void send_packet(const uint8_t *data, size_t length) {
+    // Add radio send logic here.
+    // For example:
+    // radio.send(data, length);
+  }
 };
 
 }  // namespace lorawan
